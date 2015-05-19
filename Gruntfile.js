8f2b6c2d3ff38ed24d152745aecb3f5075f5dacc
Vipin Nair <vipinsthename@gmail.com>
2015-05-19 01:12:27 +0100

    Added wercker file

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,65 +1,8 @@
 module.exports = function(grunt) {
 
   grunt.initConfig({
-    aws: grunt.file.readJSON('aws.json'),
 
     clean: ['public'],
-
-    s3: {
-      options: {
-        key: '<%= aws.key %>',
-        secret: '<%= aws.secret %>',
-        bucket: '<%= aws.bucket %>',
-        access: 'public-read'
-      },
-
-      all: {
-        upload: [{
-          dest: '',
-          src: 'public/**/*',
-          rel: 'public/'
-        }]
-      },
-
-      css: {
-        upload: [{
-          dest: '',
-          src: 'public/**/*.css',
-          rel: 'public/',
-          options: {
-            headers: {
-              'Content-Type': 'text/css'
-            }
-          }
-        }]
-      },
-
-      html: {
-        upload: [{
-          dest: '',
-          src: 'public/**/*.html',
-          rel: 'public/',
-          options: {
-            headers: {
-              'Content-Type': 'text/html; charset=utf-8'
-            }
-          }
-        }],
-      },
-
-      js: {
-        upload: [{
-          dest: '',
-          src: 'public/**/*.js',
-          rel: 'public/',
-          options: {
-            headers: {
-              'Content-Type': 'text/javascript'
-            }
-          }
-        }]
-      }
-    },
 
     shell: {
       generate: {
@@ -73,11 +16,9 @@ module.exports = function(grunt) {
   });
 
   grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-s3');
   grunt.loadNpmTasks('grunt-shell');
 
   grunt.registerTask('generate', ['clean', 'shell:generate']);
   grunt.registerTask('server', ['shell:server']);
-  grunt.registerTask('deploy', ['generate', 's3']);
   grunt.registerTask('default', ['server']);
 };
